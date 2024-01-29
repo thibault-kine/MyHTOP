5aba6dc1aa249122dd589fcb4ea334c43134075d
thibault-kine <91733294+thibault-kine@users.noreply.github.com>
2024-01-29 14:17:16 +0100

    style: centered title

--- main.c
+++ main.c
@@ -1,12 +1,30 @@
 #include <stdlib.h>
 #include <ncurses.h>
+#include <string.h>
+#include <sys/resource.h>
+#include <sys/time.h>
 
 int main() {
+
+    WINDOW *main, *header;
     initscr();
-    printw(":: myHTOP ::\nConstruction en cours...");
-    refresh();
+
+    const char* TITLE = ":: myHTOP v0.1 ::";
+    const char* SUB = "Construction en cours...";
+
+    const double HEADER_W = strlen(SUB) + 6;
+
+    main = subwin(stdscr, LINES, COLS, 0, 0);
+    header = subwin(main, 5, HEADER_W, 0, (COLS - HEADER_W) / 2);
+    mvwprintw(header, 1, (HEADER_W - strlen(TITLE)) / 2, "%s", TITLE);
+    mvwprintw(header, 3, (HEADER_W - strlen(SUB)) / 2, "%s", SUB);
+
+    box(header, ACS_VLINE, ACS_HLINE);
+    wrefresh(header);
     getch();
     endwin();
+
+    free(header);
 
     return 0;
 }
